7a1ecae84bc903b35fce5ff46217ddaa72971eac
YuTung Liu <yu.t.200053@gmail.com>
2024-03-14 01:31:07 +0800

    user_UserCart test

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 import 'bootstrap/scss/bootstrap.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
 // 導入 vue axios，可以直接用 this.axios
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+
+// 導入 Loading Overlay
+import Loading from 'vue-loading-overlay';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
@@ -19,6 +23,10 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
 app.use(VueAxios, axios);
+
+// 註冊 loading 元件
+app.component('VueLoading', Loading);
 
 app.mount('#app');
--- src/views/user/UserCart.vue
+++ src/views/user/UserCart.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>購物車</div>
+  <section class="container cart">
+    <h2 class="text-center fw-bold">前台購物車</h2>
+    {{ carts.carts[0] }}
+  </section>
 </template>
+
+<script>
+const { VITE_URL, VITE_PATH } = import.meta.env;
+
+export default {
+  data() {
+    return {
+      carts: {}
+    };
+  },
+  methods: {
+    getCart() {
+      const api = `${VITE_URL}/api/${VITE_PATH}/cart`;
+      this.axios
+        .get(api)
+        .then((res) => {
+          this.carts = res.data.data;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    }
+  },
+  mounted() {
+    this.getCart();
+  }
+};
+</script>
