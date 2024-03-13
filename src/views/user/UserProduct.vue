21e32539fed50eaee0d8dcf49204a53d9838b30e
YuTung Liu <yu.t.200053@gmail.com>
2024-03-14 00:26:40 +0800

    user_UserProduct addCart

--- src/views/user/UserProduct.vue
+++ src/views/user/UserProduct.vue
@@ -11,7 +11,7 @@
         <h1 class="title mb-0">{{ product.title }}</h1>
       </div>
       <!-- category -->
-      <div class="text-center mb-5">
+      <div class="text-center mb-3">
         <a
           href="#"
           class="badge rounded-pill btn btn-primary text-white me-1"
@@ -21,12 +21,18 @@
           {{ `#${category}` }}
         </a>
       </div>
+      <!-- area -->
+      <div class="text-center mb-5 fs-5">
+        <a href="#" class="link-dark-green">
+          <i class="bi bi-geo-alt-fill me-1"></i>{{ product.unit }}
+        </a>
+      </div>
     </div>
   </section>
   <!-- product-detail -->
   <section class="container pb-5">
     <div class="row">
-      <div class="col-12 col-lg-8">
+      <div class="col-12 col-lg-7">
         <!-- description -->
         <div class="product-description p-5 mx-auto mb-5">
           <div>
@@ -60,14 +66,34 @@
         </div>
       </div>
       <!-- add-cart -->
-      <div class="col-12 col-lg-4 product-add-cart">
+      <div class="col-12 col-lg-5 product-add-cart">
         <div class="sticky-top">
           <div class="product-description p-5 mx-auto mb-6">
             <div>
-              <h3 class="title mb-0">加購物車</h3>
-              <span class="english fs-5 title">Features</span>
+              <h4 class="fw-bold">{{ product.title }}</h4>
+              <div class="my-4">
+                <del class="text-muted">
+                  原價
+                  {{ product.origin_price ? product.origin_price.toLocaleString('en-US') : '' }} 元
+                </del>
+                <br />
+                <span class="fs-5 fw-bold mb-0 text-danger">
+                  限時特價 {{ product.price ? product.price.toLocaleString('en-US') : '' }} 元
+                </span>
+              </div>
             </div>
-            <div class="mt-4 text-muted ls-2">{{ product.description }}</div>
+            <div class="my-4">
+              <select class="form-select" aria-label="Default select example" v-model="qty">
+                <option v-for="i in 10" :key="i" :value="i">{{ i }} 張</option>
+              </select>
+            </div>
+            <button
+              type="submit"
+              class="btn btn-primary text-white w-100"
+              @click.prevent="addCart(product.id, qty)"
+            >
+              <i class="bi bi-bag me-1"></i>加入購物車
+            </button>
           </div>
         </div>
       </div>
@@ -81,10 +107,27 @@ const { VITE_URL, VITE_PATH } = import.meta.env;
 export default {
   data() {
     return {
-      product: {}
+      product: {},
+      qty: 1
     };
   },
-  methods: {},
+  methods: {
+    addCart(product_id, qty = 1) {
+      const order = { product_id, qty };
+      console.log(order);
+      const api = `${VITE_URL}/api/${VITE_PATH}/cart`;
+
+      this.axios
+        .post(api, { data: order })
+        .then((res) => {
+          alert(res.data.message);
+          console.log(order);
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    }
+  },
   mounted() {
     const { id } = this.$route.params;
     const api = `${VITE_URL}/api/${VITE_PATH}/product/${id}`;
