2865d91f098083009720ef305505d956a0c4a813
YuTung Liu <yu.t.200053@gmail.com>
2024-03-14 02:18:21 +0800

    user_UserCart layout

--- src/views/user/UserCart.vue
+++ src/views/user/UserCart.vue
@@ -1,6 +1,35 @@
 <template>
-  <section class="container cart">
-    <h2 class="text-center fw-bold">前台購物車</h2>
+  <!-- banner  -->
+  <section class="cart-banner img-box mb-5">
+    <div class="container">
+      <!-- title -->
+      <div class="cart-banner-title text-center w-100">
+        <h1 class="title mb-0">購物車</h1>
+        <span class="english fs-5 title">Cart</span>
+      </div>
+    </div>
+  </section>
+  <!-- cart -->
+  <section class="container">
+    <div class="row">
+      <div class="col-8">
+        <!-- cart list -->
+        <div class="product-description p-5 mx-auto mb-5">
+          <div>
+            <h3 class="title mb-0">123</h3>
+            <span class="english fs-5 title">123</span>
+          </div>
+          <div class="mt-4 text-muted ls-2">購物車清單、刪除單一&全部、調整數量</div>
+        </div>
+      </div>
+      <!-- cart price -->
+      <div class="col-4">
+        <div class="product-description p-5 mx-auto mb-5">總計</div>
+      </div>
+    </div>
+  </section>
+  <!-- order information -->
+  <section class="container">
     <VeeForm v-slot="{ errors }" @submit="onSubmit">
       <VeeField
         id="email"
