cdabf72b2b0a6c1ce0848b59fb230e8dd2b1b069
YuTung Liu <yu.t.200053@gmail.com>
2024-04-05 20:58:24 +0800

    Updates

---
+++ assets/NotFound-CBNbUtcs.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,o as s,a as e}from"./index-BEUrjnwo.js";const c={},n={class:"container text-center notFound"},a=e("h1",{class:"fw-bold text-dark-green"},"404",-1),r=e("p",{class:"fw-normal text-muted ls-2"},"找不到此頁，可能掉進樹洞裡囉~",-1),_=[a,r];function d(l,i){return s(),o("div",n,_)}const h=t(c,[["render",d]]);export{h as default};
